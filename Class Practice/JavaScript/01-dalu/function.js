a38a883980719a0a18f6b86e6d90a50250cbc125
Jude Chuks <judechuks26@gmail.com>
2024-01-10 13:42:44 +0100

    Javascript class

---
+++ Class Practice/JavaScript/01-dalu/function.js
@@ -0,0 +1,28 @@
+function areaOfATriangle(a, b) {
+  // parameter
+  const area = 0.5 * a * b;
+  return area;
+}
+
+console.log(areaOfATriangle(6, 9)); // argument
+
+function quadracticEquation(a, b, c) {
+  // formular = (-b+-sqrt(-b*-b - 4 * a * c)) / 2 * a
+  const x1 = ((-b + Math.sqrt(-b * -b - 4 * a * c)) / 2) * a;
+  const x2 = ((-b - Math.sqrt(-b * -b - 4 * a * c)) / 2) * a;
+  console.log("first value", x1);
+  console.log("second value", x2);
+}
+
+quadracticEquation(2, 3, 1);
+// Math.sqrt(4) // to check for square root
+// Math.pow(4, 2); // to check for raised to the power
+
+// Arrow function
+const dalu = (a, b, c) => {
+  const x1 = ((-b + Math.sqrt(-b * -b - 4 * a * c)) / 2) * a;
+  const x2 = ((-b - Math.sqrt(-b * -b - 4 * a * c)) / 2) * a;
+  console.log("first value", x1);
+  console.log("second value", x2);
+};
+dalu(2, 3, 6);
---
+++ Class Practice/JavaScript/01-dalu/intro.js
@@ -0,0 +1,16 @@
+var x; // decaration
+x = 2; // definition, assignment operator
+console.log("x is equal to " + x);
+let y = "4";
+console.log("y is equal to ", y);
+let z = x + y;
+console.log(typeof y);
+
+let a = prompt("Enter a number");
+// a = Number(a);
+// a = a.toString();
+// a = parseInt(a);
+// a = parseFloat(a);
+let b = a * 2;
+console.log(b);
+console.log(typeof a);

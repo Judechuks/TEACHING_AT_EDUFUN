cba468a87ec2f9ed2591ddb10f00a352a0aefb63
Jude Chuks <judechuks26@gmail.com>
2024-01-19 19:17:29 +0100

    do-while loop

---
+++ Class Practice/JavaScript/01-dalu/loop.js
@@ -0,0 +1,13 @@
+// const myHeading = document.getElementById("heading");
+let a = 0,
+  i = 10;
+
+do {
+  // myHeading.innerText +=  1
+  console.log(a);
+  i--;
+} while (i > 1);
+
+// Assignment
+// write a loop to console log odd numbers from 1 to 20
+// write a loop to store an array of odd numbers from 1 to 20, and console log the results

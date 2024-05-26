8558c0a4afaf5dc51c4bb1949395b127a1b1e236
Jude Chuks <judechuks26@gmail.com>
2024-05-26 17:01:05 +0100

    arithmetic operations in javascript

---
+++ Class Practice/JavaScript/01-dalu/arithmetic-operation.js
@@ -0,0 +1,15 @@
+let a = 10;
+var b = 21;
+let c = 0;
+
+// addition +
+c = a + b;
+console.log("addition", c);
+
+// multiplication *
+c = a * b;
+console.log("multiplication", c);
+
+// modulus (remainder) %
+c = b % a;
+console.log("modulus", c);

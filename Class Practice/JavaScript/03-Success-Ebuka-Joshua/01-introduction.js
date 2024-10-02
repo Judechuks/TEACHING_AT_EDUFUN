517f630fc8023d68b78445628a7dcfcb1ca234c1
Jude Chuks <judechuks26@gmail.com>
2024-10-02 14:56:38 +0100

    JavaScript Introductory class

---
+++ Class Practice/JavaScript/03-Success-Ebuka-Joshua/01-introduction.js
@@ -0,0 +1,8 @@
+// my_button = document.getElementById("my-button");
+// my_button.addEventListener("click", () => alert("Welcome to my website!"));
+
+// my_name = "Success" // String datatype
+// var my_name = 'Success'; // String datatype
+let my_name = "Success"; // String datatype
+const gender = "Male"; // String
+const isMarried = true;

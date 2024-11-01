26a4939f3392a63a060db4f04ac093748f416ba1
Jude Chuks <judechuks26@gmail.com>
2024-11-01 18:37:42 +0100

    variables in js

---
+++ Class Practice/JavaScript/02-Nonso/02-variable.js
@@ -0,0 +1,9 @@
+let name = "Nonso";
+var age = 20;
+let country = "Nigeria";
+const pi = 22 / 7;
+let radius = 5;
+var state = null;
+let area_of_circle = pi * radius * radius;
+
+console.log("Area of a circle with radius", radius, "is:", area_of_circle);

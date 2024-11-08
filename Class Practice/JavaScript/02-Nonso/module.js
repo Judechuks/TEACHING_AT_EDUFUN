e90858253a0097b847f21297e9e46dd5321d0f54
Jude Chuks <judechuks26@gmail.com>
2024-11-08 17:29:59 +0100

    module - import and export in js

---
+++ Class Practice/JavaScript/02-Nonso/module.js
@@ -0,0 +1,9 @@
+// import { name as fullname, age } from "./Module/index.js";
+// alert(fullname);
+
+// import message, { showDetails } from "./Module/index.js";
+
+// Importing everything
+import * as all from "./Module/index.js";
+all.message();
+all.showDetails();
